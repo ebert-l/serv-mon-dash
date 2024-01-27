edbe842d9c4aca60973b9e80535b67adc4fc9940
ebert.l <info@leon-ebert.de>
2024-01-27 19:43:49 +0100

    Frontend for Dashboard generated

--- dashboard/templates/dashboard/includes/dash/dash.js
+++ dashboard/templates/dashboard/includes/dash/dash.js
@@ -11,9 +11,38 @@ fetchFromAPI();
 
 function handleJSON(json){
     // write to cpu percent
+    console.log(json);
     let cpu_panel = document.getElementById("CPU-Auslastung");
-    cpu_panel.querySelector(".h5").innerText = `${json.cpu_percent}%`;
+    let cpuValue = json.cpu_percent.toLocaleString('de-DE', {
+        minimumFractionDigits: 1,
+        maximumFractionDigits: 1
+      })
+    cpu_panel.querySelector(".h5").innerText = `${cpuValue}%`;
     cpu_panel.querySelector('[role="progressbar"]').style.width = `${json.cpu_percent}%`;
+
+    let ram_panel = document.getElementById("RAM-Auslastung");
+    let ramValue = json.memory_percent.toLocaleString('de-DE', {
+        minimumFractionDigits: 1,
+        maximumFractionDigits: 1
+      })
+    ram_panel.querySelectorAll(".h5")[0].innerText = `${ramValue}%`;
+    ram_panel.querySelectorAll(".h5")[1].innerText = `${json.memory}GB`;
+    ram_panel.querySelector('[role="progressbar"]').style.width = `${json.memory_percent}%`;
+
+    let volumes_panel = document.getElementById("Festplatten");
+    let volumesArray = [];
+    for(let i = 0; i < json.drives_usage.length; i++){
+        let name = json.drives_usage[i].name;
+        let size = json.drives_usage[i].total.toLocaleString('de-DE', {
+            maximumFractionDigits: 0
+          });
+        volumesArray.push(`${name}&nbsp;(${size}&nbsp;GB)`);
+    }
+    volumes_panel.querySelectorAll(".h5")[0].innerHTML = `${volumesArray.join(", ")}`;
+
+    let tasks_panel = document.getElementById("Tasks");
+    // Todo: Ersetzen durch Anzahl Tasks
+    tasks_panel.querySelectorAll(".h5")[0].innerText = `${json.memory_free}`;
 
 
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,49 +7,14 @@ import psutil, os
 # def index(resp):
 #     return render(resp, "dashboard/dash.html", {})
 def index(resp):
-    cpu_percent = psutil.cpu_percent()
-    # cpu_time = psutil.cpu_times()
-    disk = psutil.disk_usage(os.getcwd())
-    memory = psutil.virtual_memory()
-    memory_total = round(memory.total/1073741824,1)
-    memory_used = round(memory.used/1073741824,1)
-    memory_free = round(memory.free/1073741824,1)
-    disk_usage = round(disk.used/1073741824,1)
-    disk_free = round(disk.free/1073741824,1)
-    ram_info = psutil.virtual_memory()
-
-
-    drives = [ chr(x) + ":" for x in range(65,91) if os.path.exists(chr(x) + ":") ]
-    drives_usage = []
-    for drive in drives:
-        drive_info = psutil.disk_usage(drive)
-        drives_usage.append(drive + " gesamt: " + str(round(drive_info[0]/1073741824,1)) + "GB, davon frei: " + str(round(drive_info[2]/1073741824,1)) + "GB")
-
-    processes = []
-    for proc in psutil.process_iter(['pid']):
-        p = psutil.Process(pid=proc.pid)
-        processes.append(p.as_dict(attrs=['pid', 'name', 'username', 'cpu_percent', 'memory_percent']))
-    top_processes = sorted(processes, key=lambda i: i['cpu_percent'], reverse=True)[:5]
-
-
-    context = {
-        'cpu_percent': cpu_percent,
-        # 'cpu_time' : cpu_time,
-        'memory': memory_total,
-        'memory_used': memory_used,
-        'memory_free': memory_free,
-        'disk_usage' : disk_usage,
-        'disk_free' : disk_free,
-        'ram_info': ram_info,
-        'drives_usage': drives_usage,
-        'top_processes': top_processes
-    }
-
+    context = gatherSystemInformation()
     return render(resp, 'dashboard/dash.html', context)
 
-
-
 def data(resp):
+    json_resp = gatherSystemInformation()
+    return JsonResponse(json_resp)
+
+def gatherSystemInformation():
     cpu_percent = psutil.cpu_percent()
     # cpu_time = psutil.cpu_times()
     disk = psutil.disk_usage(os.getcwd())
@@ -57,9 +22,9 @@ def data(resp):
     memory_total = round(memory.total/1073741824,1)
     memory_used = round(memory.used/1073741824,1)
     memory_free = round(memory.free/1073741824,1)
+    memory_percent = memory.percent
     disk_usage = round(disk.used/1073741824,1)
     disk_free = round(disk.free/1073741824,1)
-    ram_info = psutil.virtual_memory()
 
 
     drives = [ chr(x) + ":" for x in range(65,91) if os.path.exists(chr(x) + ":") ]
@@ -81,12 +46,10 @@ def data(resp):
         'memory': memory_total,
         'memory_used': memory_used,
         'memory_free': memory_free,
+        'memory_percent': memory_percent,
         'disk_usage' : disk_usage,
         'disk_free' : disk_free,
-        'ram_info': ram_info,
         'drives_usage': drives_usage,
         'top_processes': top_processes
     }
-
-    return JsonResponse(json_resp)
-
+    return json_resp
